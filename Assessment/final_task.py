334c7ed8a4f2c80fedb5faeabf83906ab844d049
MSedan <merily.takel@gmail.com>
2025-10-17 09:48:17 +0100

    first commit

---
+++ Assessment/final_task.py
@@ -0,0 +1,44 @@
+#Final assessment of the module
+
+#Mass is in kilograms
+mass ={"Mercury": 3.3011*10**23,
+       "Venus": 4.8675*10**24,
+       "Earth": 5.972168*10**24,
+       "Mars": 6.4171*10**23,
+       "Jupiter": 1.8982*10**27,
+       "Saturn": 5.6834*10**26,
+       "Uranus": 8.6810*10**25,
+       "Neptune": 1.02409*10**26
+       }
+
+#Distance from the sun is in kilometers
+distance_from_sun = {"Mercury": 46000000,
+                     "Venus": 108000000,
+                     "Earth": 149600000,
+                     "Mars": 230000000,
+                     "Jupiter": 778000000,
+                     "Saturn": 1434000000,
+                     "Uranus": 2000000000,
+                     "Neptune": 4500000000
+                     }
+
+#Moons
+
+number_of_moons = {"Mercury": 0,
+         "Venus": 0,
+         "Earth": 1,
+         "Mars": 2,
+         "Jupiter": 97,
+         "Saturn": 274,
+         "Uranus": 29,
+         "Neptune": 16
+         }
+
+earth_moons = ["Moon"]
+mars_moons = ["Phobos", "Deimos"]
+jupiter_moons = ["Io", "Europa", "Ganymede", "Callisto", "Themisto"]
+saturn_moons = ["Titan", "Rhea", "Pandora", "Prometheus", "Enceladus"]
+uranus_moons = ["Miranda", "Ariel", "Umbriel", "Titania", "Oberon"] 
+neptune_moons = ["Triton", "Nereid", "Proteus", "Naiad", "Thalassa"]
+
+

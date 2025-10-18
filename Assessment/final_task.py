58ae8883d9087e3093cf7d2a6359e3ede74b556e
MSedan <merily.takel@gmail.com>
2025-10-18 22:01:52 +0100

    Classes and frame 1

--- Assessment/final_task.py
+++ Assessment/final_task.py
@@ -1,97 +1,86 @@
-#Final assessment of the module
+#Final assessment of the module Fundamentals of Computing
 import tkinter as tk
 from tkinter import *
 
 #Data for the planets
 #Mass is in kilograms
-mass ={"Mercury": 3.3011*10**23,
-       "Venus": 4.8675*10**24,
-       "Earth": 5.972168*10**24,
-       "Mars": 6.4171*10**23,
-       "Jupiter": 1.8982*10**27,
-       "Saturn": 5.6834*10**26,
-       "Uranus": 8.6810*10**25,
-       "Neptune": 1.02409*10**26
-       }
 
-#Distance from the sun is in kilometers
-distance_from_sun = {"Mercury": 46000000,
-                     "Venus": 108000000,
-                     "Earth": 149600000,
-                     "Mars": 230000000,
-                     "Jupiter": 778000000,
-                     "Saturn": 1434000000,
-                     "Uranus": 2000000000,
-                     "Neptune": 4500000000
-                     }
+class Planet:
+       def __init__(self, name, mass, distance_from_sun, number_of_moons, names_of_moons):
+              self.name = name
+              self.mass = mass
+              self.distance_from_sun = distance_from_sun
+              self.number_of_moons = number_of_moons
+              self.names_of_moons = names_of_moons
+              
+       def get_name(self):
+              return self.name
+       def get_mass(self):
+              return self.mass
+       def get_distance_from_sun(self):
+              return self.distance_from_sun
+       def get_number_of_moons(self):
+              return self.number_of_moons
+       def get_names_of_moons(self):      
+              return self.names_of_moons
 
-#Moons
-number_of_moons = {"Mercury": 0,
-         "Venus": 0,
-         "Earth": 1,
-         "Mars": 2,
-         "Jupiter": 97,
-         "Saturn": 274,
-         "Uranus": 29,
-         "Neptune": 16
-         }
+mercury = Planet("Mercury", 3.3011*10**23, 46000000, 0, [])
+venus = Planet("Venus", 4.8675*10**24, 108000000, 0, [])
+earth = Planet("Earth", 5.972168*10**24, 149600000, 1, ["Moon"])
+mars = Planet("Mars", 6.4171*10**23, 230000000, 2, ["Phobos", "Deimos"])
+jupiter = Planet("Jupiter", 1.8982*10**27, 778000000, 97, ["Io", "Europa", "Ganymede", "Callisto", "Themisto"])
+saturn = Planet("Saturn", 5.6834*10**26, 1434000000, 274, ["Titan", "Rhea", "Pandora", "Prometheus", "Enceladus"])
+uranus = Planet("Uranus", 8.6810*10**25, 2000000000, 29, ["Miranda", "Ariel", "Umbriel", "Titania", "Oberon"])
+neptune = Planet("Neptune", 1.02409*10**26, 4500000000, 16, ["Triton", "Nereid", "Proteus", "Naiad", "Thalassa"])
 
-#Names of moons
-earth_moons = ["Moon"]
-mars_moons = ["Phobos", "Deimos"]
-jupiter_moons = ["Io", "Europa", "Ganymede", "Callisto", "Themisto"]
-saturn_moons = ["Titan", "Rhea", "Pandora", "Prometheus", "Enceladus"]
-uranus_moons = ["Miranda", "Ariel", "Umbriel", "Titania", "Oberon"] 
-neptune_moons = ["Triton", "Nereid", "Proteus", "Naiad", "Thalassa"]
-
-#List of valid planets
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets = [mercury.get_name(), venus.get_name(), earth.get_name(), mars.get_name(), jupiter.get_name(),saturn.get_name(), uranus.get_name(), neptune.get_name()]
 
 
-def check_if_planet():
-    planet = entry.get().lower().capitalize()
-    if planet in planets:
-        show_frame2()
-    else:
-       wrong_planet = tk.Label(frame1, text = planet + " is not a planet or you have made a typo", font="20", fg="red")
-       wrong_planet.pack(pady=10)
-       
-       
-#Functions to switch between frames        
+#Function for user input
+def get_entry(entry):
+       planet = entry.get().lower().capitalize()
+       return planet
+
+#Function to check the input
+def check_if_planet(planet):
+       if planet in planets:
+              show_frame2()
+       elif planet == "":
+              empty_label = tk.Label(frame1, text="Please enter a planet name.")
+              empty_label.pack()
+       else:
+              wrong_planet = tk.Label(frame1, text= planet + " is not a planet or you have made a typo. Please try again.")
+              wrong_planet.pack()
+
+#Functions to switch between frames
 def show_frame2():
        frame1.pack_forget()
        frame2.pack()
+
 def return_to_frame1():
        frame2.pack_forget()
        frame1.pack()
 
 
-
-
-#Used this page to help with Tkinter https://www.geeksforgeeks.org/python/python-tkinter-tutorial/ 
+#Used this page to help with Tkinter https://www.geeksforgeeks.org/python/python-tkinter-tutorial/
 #Creating the main window
 window = Tk()
 window.title("Solar system planets")
 window.geometry("500x500")
 
-#First window frame
+#First frame
 frame1 = tk.Frame(window)
 frame1.pack()
 label_main_text = tk.Label(frame1, text = "Please type a planet you would like to learn about:", font="50")
 label_main_text.pack(pady=20)
 entry = tk.Entry(frame1, width = 40)
 entry.pack(pady=10)
-check_button = tk.Button(frame1, text = "Submit", command=check_if_planet)
+check_button = tk.Button(frame1, text = "Submit", command=lambda: check_if_planet(get_entry(entry)),
+                         cursor = "hand2")
 check_button.pack(pady=10)
 
-#Second window frame
+#Second frame
 frame2 = tk.Frame(window)
-label2 = tk.Label(frame2, text = f"You have picked {entry.get()}", font="50")
-label2.pack(pady=20)
-
-
-
-
 button_all_information = tk.Button(frame2, text = "Tell me everything you know",
                          cursor = "hand2"
                      )

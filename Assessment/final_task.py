fd0b9cecc7be53fa391f95d96582e294f7cb77c7
MSedan <merily.takel@gmail.com>
2025-10-19 18:20:28 +0100

    Finalising buttons and working on labels

--- Assessment/final_task.py
+++ Assessment/final_task.py
@@ -33,17 +33,19 @@ saturn = Planet("Saturn", 5.6834*10**26, 1434000000, 274, ["Titan", "Rhea", "Pan
 uranus = Planet("Uranus", 8.6810*10**25, 2000000000, 29, ["Miranda", "Ariel", "Umbriel", "Titania", "Oberon"])
 neptune = Planet("Neptune", 1.02409*10**26, 4500000000, 16, ["Triton", "Nereid", "Proteus", "Naiad", "Thalassa"])
 
-planets = [mercury.get_name(), venus.get_name(), earth.get_name(), mars.get_name(), jupiter.get_name(),saturn.get_name(), uranus.get_name(), neptune.get_name()]
-
+valid_planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
+current_planet = None
 
 #Function for user input
 def get_entry(entry):
        planet = entry.get().lower().capitalize()
        return planet
 
-#Function to check the input
+#Function to check the input and move to frame 2
 def check_if_planet(planet):
-       if planet in planets:
+       global current_planet
+       if planet in [p.get_name() for p in valid_planets]:
+              current_planet = planet
               show_frame2()
        elif planet == "":
               empty_label = tk.Label(frame1, text="Please enter a planet name.")
@@ -51,6 +53,62 @@ def check_if_planet(planet):
        else:
               wrong_planet = tk.Label(frame1, text= planet + " is not a planet or you have made a typo. Please try again.")
               wrong_planet.pack()
+
+#Get mass of the planet
+def get_planet_mass(current_planet):
+       for planet in valid_planets:
+              if planet.get_name() == current_planet:
+                     mass = planet.get_mass()
+                     return mass
+
+#Function to display mass
+def display_mass():
+       mass = get_planet_mass(current_planet)
+       mass_label.config(text="The mass of " + str(current_planet) + " is " + str(mass) + " kg.")
+       mass_label.pack()
+       
+#Function to get distance from the sun
+def get_distance_from_sun(current_planet):
+       for planet in valid_planets:
+              if planet.get_name() == current_planet:
+                     distance = planet.get_distance_from_sun()
+                     return distance
+
+#Function to display distance from the sun
+def display_distance():
+       distance = get_distance_from_sun(current_planet)
+       distance_label.config(text="The distance of " + str(current_planet) + " from the sun is " + str(distance) + " km.")
+       distance_label.pack()
+
+#Funtion to get number of moons
+def get_number_of_moons(current_planet):
+       for planet in valid_planets:
+              if planet.get_name() == current_planet:
+                     number_of_moons = planet.get_number_of_moons()
+                     return number_of_moons
+              
+#Function to display number of moons
+def display_number_of_moons():
+       number_of_moons = get_number_of_moons(current_planet)
+       number_moons_label.config(text="The number of moons of " + str(current_planet) + " is " + str(number_of_moons) + ".")
+       number_moons_label.pack()
+
+#Function to get names of moons
+def get_names_of_moons(current_planet):
+       for planet in valid_planets:
+              if planet.get_name() == current_planet:
+                     names_of_moons = planet.get_names_of_moons()
+                     return names_of_moons
+              
+#Function to display names of moons
+def display_names_of_moons():
+       names_of_moons = get_names_of_moons(current_planet)
+       if names_of_moons:
+              names_moons_label.config(text="The names of the moons of " + str(current_planet) + " are: " + ", ".join(names_of_moons) + ".")
+       else:
+              names_moons_label.config(text=str(current_planet) + " has no moons.")
+       names_moons_label.pack()
+       
 
 #Functions to switch between frames
 def show_frame2():
@@ -79,28 +137,36 @@ check_button = tk.Button(frame1, text = "Submit", command=lambda: check_if_plane
                          cursor = "hand2")
 check_button.pack(pady=10)
 
-#Second frame
+#Creating the second frame
 frame2 = tk.Frame(window)
-button_all_information = tk.Button(frame2, text = "Tell me everything you know",
-                         cursor = "hand2"
-                     )
-button_all_information.pack(pady=10)
+label_main_text = tk.Label(frame2, text = "You have picked " + str(get_entry(entry)), font="50")
+label_main_text.pack(pady=20)
 
-button_mass = tk.Button(frame2, text = "Mass",
+#Buttons and labels for frame 2
+#Used this page to help with Tkinter labels https://coderslegacy.com/python/tkinter-config/
+button_mass = tk.Button(frame2, text = "Mass", command= lambda: display_mass(),
                          cursor = "hand2")
 button_mass.pack(pady=10)
+mass_label = tk.Label(frame2, text="")
+mass_label.pack()
 
-button_distance = tk.Button(frame2, text = "Distance from the sun",
+button_distance = tk.Button(frame2, text = "Distance from the sun", command= display_distance(),
                          cursor = "hand2")
 button_distance.pack(pady=10)
+distance_label = tk.Label(frame2, text="")
+distance_label.pack()
 
-button_number_moons = tk.Button(frame2, text = "Number of moons",
+button_number_moons = tk.Button(frame2, text = "Number of moons", command= display_number_of_moons(),
                             cursor = "hand2")
 button_number_moons.pack(pady=10)
+number_moons_label = tk.Label(frame2, text="")
+number_moons_label.pack()
 
-button_names_moons = tk.Button(frame2, text = "Name of moons",
+button_names_moons = tk.Button(frame2, text = "Name of moons", command= display_names_of_moons,
                           cursor = "hand2")
 button_names_moons.pack(pady=10)
+names_moons_label = tk.Label(frame2, text="")
+names_moons_label.pack()
 
 button_new_planet = tk.Button(frame2, text = "Choose a new planet",
                           cursor = "hand2", command= return_to_frame1)
